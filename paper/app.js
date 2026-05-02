5f6411734257475a52d8ff444269ff56e54d4f19
amnaqamartheplayer-lang <amnaqamar.theplayer@gmail.com>
2026-05-02 18:45:48 +0500

    Add files via upload

---
+++ paper/app.js
@@ -0,0 +1,87 @@
+//task 01
+
+// var userName = prompt("Enter your name")
+// // alert(userName)
+// var math = +prompt("Enter your Math's marks")
+// // alert (math) 
+// var science = +prompt("Enter your Science's marks")
+// // alert (science) 
+// var english = +prompt("Enter your English's marks")
+// // alert (english) 
+// var obtain = math + science + english
+// var total = 300
+// alert(obtain)
+// var res = (obtain / total) * 100
+// // alert=( res )
+
+
+// if (res >= 80) {
+//     alert("Excellent")
+
+// } 
+// else if (res >= 60 && res <= 79) {
+//     alert("GOOD")
+// }
+// else if (res >= 59){
+//     alert("avg")
+// }
+// else if (res < 40){
+//     alert("fail")
+// }
+// else{
+//     alert("Invalid number")
+// }
+
+
+//task 02
+
+// var userLoginName = 'admin'
+// var userLoginPass = 12345
+
+// var userName = prompt("Enter your Name")
+// var userPass = prompt("Enter your pass")
+
+// if(userLoginName === userName){
+//     if(userPass == userLoginPass){
+//         alert("Login Success")
+//     }
+//     else{
+//         alert("Password not match")
+//     }
+// }
+// else{
+//     alert("Invalid username")
+// }
+
+
+//task 03
+
+// var num = prompt("Enter your number")
+
+// if(num % 2==0){
+//     alert("Its an Even")
+// }
+// else{
+//     alert("odd")
+// }
+
+//task 04
+
+// var userAge = prompt("Enter your Age")
+// // alert(userAge)
+
+// if(userAge <= 5){
+//     alert("Free Ticket")
+// }
+// else if(userAge >= 6 && userAge <= 12){
+//     alert("Ticket price 100")
+// }
+// else if(userAge >= 13 && userAge <=59){
+//     alert("Ticket price 300")
+// }
+// else if(userAge <= 60){
+//     alert("Ticket price 150")
+// }
+// else{
+//     alert("user age not valid")
+// }
